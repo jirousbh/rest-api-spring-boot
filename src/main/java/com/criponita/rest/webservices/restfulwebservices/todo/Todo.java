efc70c85324ab9d802812fd19859515226b0a960
jirousbh <jirous@gmail.com>
2020-03-17 15:54:24 -0300

    New resources put,post,delete

--- src/main/java/com/criponita/rest/webservices/restfulwebservices/todo/Todo.java
+++ src/main/java/com/criponita/rest/webservices/restfulwebservices/todo/Todo.java
@@ -8,7 +8,11 @@ public class Todo {
 	private String description;
 	private Date targetDate;
 	private boolean isdone;
+	
+	protected Todo() {
 		
+	}
+	
 	public Todo(long id, String username, String description, Date targetDate, boolean isdone) {
 		super();
 		this.id = id;
@@ -46,6 +50,26 @@ public class Todo {
 	}
 	public void setIsdone(boolean isdone) {
 		this.isdone = isdone;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		if (id != other.id)
+			return false;
+		return true;
 	}
 	
 	
